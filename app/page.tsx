d4f726c3ff827d2bfde221e1c9ede7baee50ba43
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-07-03 02:49:10 +0500

    Fix: Unused npm package removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
 "use client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LoginScreen from "../components/LoginScreen";
 import Dashboard from "../components/Dashboard";
 
-const queryClient = new QueryClient();
-
 export default function Home() {
   return (
-    <QueryClientProvider client={queryClient}>
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginScreen />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </BrowserRouter>
-  </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
